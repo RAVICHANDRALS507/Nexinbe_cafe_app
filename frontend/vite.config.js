e3f2e7d1416438a6fe3a7a64293a28ec3d3a0d25
RAVICHANDRA <cravi7715@Gmail.com>
2025-04-05 12:20:32 +0530

    morion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ['framer-motion'], // Add framer-motion to external dependencies
-    },
-  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'framer-motion': 'framer-motion/dist/framer-motion'
+    }
+  }
 })
